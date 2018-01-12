a55a1bbc95b7bc3e21ff150dbec7997a47cc165c
angellababy <angella_baby@163.com>
2018-01-12 04:43:47 -0800

    put the mcs into the upper 8M of external Flash

--- fpga/artydevkit/script/cfgmem.tcl
+++ fpga/artydevkit/script/cfgmem.tcl
@@ -2,9 +2,9 @@ lassign $argv mcsfile bitfile datafile
 
 set iface spix4
 set size 16
-set bitaddr 0x0
+set bitaddr 0x800000
 
 write_cfgmem -format mcs -interface $iface -size $size \
   -loadbit "up ${bitaddr} ${bitfile}" \
-  -loaddata [expr {$datafile ne "" ? "up 0x400000 ${datafile}" : ""}] \
+  -loaddata [expr {$datafile ne "" ? "up 0x000000 ${datafile}" : ""}] \
   -file $mcsfile -force
