b2e65bfcf47f1515fbf54b80802b76b7d6c1d42d
angellababy <angella_baby@163.com>
2018-01-12 23:27:53 -0800

    add the option to force X to zero to easy simulation

--- rtl/e203/general/sirv_sim_ram.v
+++ rtl/e203/general/sirv_sim_ram.v
@@ -33,7 +33,9 @@
 // ====================================================================
 module sirv_sim_ram 
 #(parameter DP = 512,
+  parameter FORCE_X2ZERO = 0,
   parameter DW = 32,
+  parameter MW = 4,
   parameter AW = 32 
 )
 (
@@ -42,17 +44,17 @@ module sirv_sim_ram
   input  [AW-1  :0] addr,
   input             cs,
   input             we,
-  input  [DW/8-1:0] wem,
-  output [DW  -1:0] dout
+  input  [MW-1:0]   wem,
+  output [DW-1:0]   dout
 );
 
     reg [DW-1:0] mem_r [0:DP-1];
     reg [AW-1:0] addr_r;
-    wire [DW/8-1:0] wen;
+    wire [MW-1:0] wen;
     wire ren;
 
     assign ren = cs & (~we);
-    assign wen = ({DW/8{cs & we}} & wem);
+    assign wen = ({MW{cs & we}} & wem);
 
 
 
@@ -66,16 +68,41 @@ module sirv_sim_ram
     end
 
     generate
-      for (i = 0; i < DW/8; i = i+1) begin :mem
-        always @(posedge clk) begin
+      for (i = 0; i < MW; i = i+1) begin :mem
+        if((8*i+8) > DW ) begin: last
+          always @(posedge clk) begin
             if (wen[i]) begin
-               mem_r[addr][8*i+:8] <= din[8*i+:8];
+               mem_r[addr][DW-1:8*i] <= din[DW-1:8*i];
             end
+          end
+        end
+        else begin: non_last
+          always @(posedge clk) begin
+            if (wen[i]) begin
+               mem_r[addr][8*i+7:8*i] <= din[8*i+7:8*i];
+            end
+          end
         end
       end
     endgenerate
 
- assign dout = mem_r[addr_r];
+  wire [DW-1:0] dout_pre;
+  assign dout_pre = mem_r[addr_r];
+
+  generate
+   if(FORCE_X2ZERO == 1) begin: force_x_to_zero
+      for (i = 0; i < DW; i = i+1) begin:force_x_gen 
+          `ifndef SYNTHESIS//{
+         assign dout[i] = (dout_pre[i] === 1'bx) ? 1'b0 : dout_pre[i];
+          `else//}{
+         assign dout[i] = dout_pre[i];
+          `endif//}
+      end
+   end
+   else begin:no_force_x_to_zero
+     assign dout = dout_pre;
+   end
+  endgenerate
 
  
 endmodule
