b8a0b1734f70814be3a2872e84b01986862b1f99
angellababy <angella_baby@163.com>
2018-01-12 19:14:11 -0800

    add I2C

--- sirv-e-sdk/bsp/env/sirv-e201-arty/platform.h
+++ sirv-e-sdk/bsp/env/sirv-e201-arty/platform.h
@@ -15,26 +15,29 @@
 #include "sifive/devices/pwm.h"
 #include "sifive/devices/spi.h"
 #include "sifive/devices/uart.h"
+#include "sifive/devices/i2c.h"
+
 
 /****************************************************************************
  * Platform definitions
  *****************************************************************************/
 
 #define TRAPVEC_TABLE_CTRL_ADDR _AC(0x00001010,UL)
-#define CLINT_CTRL_ADDR _AC(0x02000000,UL)
-#define PLIC_CTRL_ADDR _AC(0x0C000000,UL)
-#define AON_CTRL_ADDR _AC(0x10000000,UL)
-#define GPIO_CTRL_ADDR _AC(0x10012000,UL)
-#define UART0_CTRL_ADDR _AC(0x10013000,UL)
-#define SPI0_CTRL_ADDR _AC(0x10014000,UL)
-#define PWM0_CTRL_ADDR _AC(0x10015000,UL)
-#define UART1_CTRL_ADDR _AC(0x10023000,UL)
-#define SPI1_CTRL_ADDR _AC(0x10024000,UL)
-#define PWM1_CTRL_ADDR _AC(0x10025000,UL)
-#define SPI2_CTRL_ADDR _AC(0x10034000,UL)
-#define PWM2_CTRL_ADDR _AC(0x10035000,UL)
-#define SPI0_MMAP_ADDR _AC(0x20000000,UL)
-#define MEM_CTRL_ADDR _AC(0x80000000,UL)
+#define CLINT_CTRL_ADDR         _AC(0x02000000,UL)
+#define PLIC_CTRL_ADDR          _AC(0x0C000000,UL)
+#define AON_CTRL_ADDR           _AC(0x10000000,UL)
+#define GPIO_CTRL_ADDR          _AC(0x10012000,UL)
+#define UART0_CTRL_ADDR         _AC(0x10013000,UL)
+#define SPI0_CTRL_ADDR          _AC(0x10014000,UL)
+#define PWM0_CTRL_ADDR          _AC(0x10015000,UL)
+#define UART1_CTRL_ADDR         _AC(0x10023000,UL)
+#define SPI1_CTRL_ADDR          _AC(0x10024000,UL)
+#define PWM1_CTRL_ADDR          _AC(0x10025000,UL)
+#define SPI2_CTRL_ADDR          _AC(0x10034000,UL)
+#define PWM2_CTRL_ADDR          _AC(0x10035000,UL)
+#define I2C_CTRL_ADDR           _AC(0x10042000,UL)
+#define SPI0_MMAP_ADDR          _AC(0x20000000,UL)
+#define MEM_CTRL_ADDR           _AC(0x80000000,UL)
 
 // IOF Mappings
 #define IOF0_SPI1_MASK          _AC(0x000007FC,UL)
@@ -70,41 +73,48 @@
 #define IOF_UART1_RX (24u)
 #define IOF_UART1_TX (25u)
 
+#define IOF0_I2C_MASK           _AC(0x0000C000, UL)
+#define IOF_I2C_SDA (14u)
+#define IOF_I2C_SCL (15u)
+
 #define IOF1_PWM0_MASK          _AC(0x0000000F, UL)
 #define IOF1_PWM1_MASK          _AC(0x00780000, UL)
 #define IOF1_PWM2_MASK          _AC(0x00003C00, UL)
 
 // Interrupt Numbers
-#define INT_RESERVED 0
-#define INT_WDOGCMP 1
-#define INT_RTCCMP 2
-#define INT_UART0_BASE 3
-#define INT_UART1_BASE 4
-#define INT_SPI0_BASE 5
-#define INT_SPI1_BASE 6
-#define INT_SPI2_BASE 7
-#define INT_GPIO_BASE 8
-#define INT_PWM0_BASE 40
-#define INT_PWM1_BASE 44
-#define INT_PWM2_BASE 48
+#define INT_RESERVED    0
+#define INT_WDOGCMP     1
+#define INT_RTCCMP      2
+#define INT_UART0_BASE  3
+#define INT_UART1_BASE  4
+#define INT_SPI0_BASE   5
+#define INT_SPI1_BASE   6
+#define INT_SPI2_BASE   7
+#define INT_GPIO_BASE   8
+#define INT_PWM0_BASE   40
+#define INT_PWM1_BASE   44
+#define INT_PWM2_BASE   48
+#define INT_I2C_BASE    52
 
 // Helper functions
-#define _REG32(p, i) (*(volatile uint32_t *) ((p) + (i)))
-#define _REG32P(p, i) ((volatile uint32_t *) ((p) + (i)))
-#define AON_REG(offset) _REG32(AON_CTRL_ADDR, offset)
-#define CLINT_REG(offset) _REG32(CLINT_CTRL_ADDR, offset)
-#define GPIO_REG(offset) _REG32(GPIO_CTRL_ADDR, offset)
-#define OTP_REG(offset)  _REG32(OTP_CTRL_ADDR, offset)
-#define PLIC_REG(offset) _REG32(PLIC_CTRL_ADDR, offset)
-#define PRCI_REG(offset) _REG32(PRCI_CTRL_ADDR, offset)
-#define PWM0_REG(offset) _REG32(PWM0_CTRL_ADDR, offset)
-#define PWM1_REG(offset) _REG32(PWM1_CTRL_ADDR, offset)
-#define PWM2_REG(offset) _REG32(PWM2_CTRL_ADDR, offset)
-#define SPI0_REG(offset) _REG32(SPI0_CTRL_ADDR, offset)
-#define SPI1_REG(offset) _REG32(SPI1_CTRL_ADDR, offset)
-#define SPI2_REG(offset) _REG32(SPI2_CTRL_ADDR, offset)
-#define UART0_REG(offset) _REG32(UART0_CTRL_ADDR, offset)
-#define UART1_REG(offset) _REG32(UART1_CTRL_ADDR, offset)
+#define _REG8(p, i)             (*(volatile uint8_t *) ((p) + (i)))
+#define _REG32(p, i)            (*(volatile uint32_t *) ((p) + (i)))
+#define _REG32P(p, i)           ((volatile uint32_t *) ((p) + (i)))
+#define AON_REG(offset)         _REG32(AON_CTRL_ADDR, offset)
+#define CLINT_REG(offset)       _REG32(CLINT_CTRL_ADDR, offset)
+#define GPIO_REG(offset)        _REG32(GPIO_CTRL_ADDR, offset)
+#define OTP_REG(offset)         _REG32(OTP_CTRL_ADDR, offset)
+#define PLIC_REG(offset)        _REG32(PLIC_CTRL_ADDR, offset)
+#define PRCI_REG(offset)        _REG32(PRCI_CTRL_ADDR, offset)
+#define PWM0_REG(offset)        _REG32(PWM0_CTRL_ADDR, offset)
+#define PWM1_REG(offset)        _REG32(PWM1_CTRL_ADDR, offset)
+#define PWM2_REG(offset)        _REG32(PWM2_CTRL_ADDR, offset)
+#define SPI0_REG(offset)        _REG32(SPI0_CTRL_ADDR, offset)
+#define SPI1_REG(offset)        _REG32(SPI1_CTRL_ADDR, offset)
+#define SPI2_REG(offset)        _REG32(SPI2_CTRL_ADDR, offset)
+#define UART0_REG(offset)       _REG32(UART0_CTRL_ADDR, offset)
+#define UART1_REG(offset)       _REG32(UART1_CTRL_ADDR, offset)
+#define I2C_REG(offset)         _REG8(I2C_CTRL_ADDR, offset)
 
 // Misc
 
